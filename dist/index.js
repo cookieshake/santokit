4ba826bee3028d01c711a97a94afc63c41020929
cookie.shake <cookie.shake@kakaocorp.com>
2026-01-13 10:48:27 +0900

    d

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import { Hono } from 'hono';
-import clientApp from './apps/client.js';
-import adminApp from './apps/admin.js';
-// This is the "Combined" app for environments that don't support multiple ports (like Cloudflare Workers standard output)
-// We mount Admin under /admin for convenience here.
-const app = new Hono();
-app.route('/admin', adminApp);
-app.route('/', clientApp);
-export default app;
